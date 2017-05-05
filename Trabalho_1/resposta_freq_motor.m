a506a31e696740b770e5fb877b137c6a8354b91c
UFRJCyfer <luizrennocosta@poli.ufrj.br>
2017-05-05 03:58:06 -0300

    commented code

--- Trabalho_1/resposta_freq_motor.m
+++ Trabalho_1/resposta_freq_motor.m
@@ -1,4 +1,5 @@
-fontsize = 12;
+
+%Define as constantes dadas no trabalho
 kt = 0.0502;
 ke = kt;
 jm = 221e-7;
@@ -7,19 +8,26 @@ ra = 10.6;
 num = kt;
 den = [ra*jm ke*kt];
 
+%Define a função de transferência
 H = tf(num,den)
+
+%Cria a figura da resposta ao degrau
 figure;
 step(H)
 fig_step=gcf;
 grid on;
-set(findall(fig_step,'-property','FontSize'),'FontSize',fontsize)
 
+%Cria o diagrama de Bode
 figure;
 bode(H)
 fig_bode=gcf;
 grid on;
+
+%Bota o tamanho das fontes das figuras em FontSize
+fontsize = 12;
+set(findall(fig_step,'-property','FontSize'),'FontSize',fontsize)
 set(findall(fig_bode,'-property','FontSize'),'FontSize',fontsize)
 
-
+%Salva as figuras do grafico de Bode e a resposta ao degrau
 saveas(fig_bode,'images/bode_response.png')
 saveas(fig_step,'images/step_response.png')
